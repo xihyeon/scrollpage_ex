b6cdfc5b5f8438198510f9b17cc0abb9db0e07fa
xihyeon <xihyeon.lee@gmail.com>
2020-11-18 17:27:26 +0900

    실무에선 addclass를 더 많이 사용함.

--- js/common.js
+++ js/common.js
@@ -1,13 +1,21 @@
 $(function(){
   //script 입력영역
-    var thisElem
-  $("nav ul li a,.footer_top a,a.button").click(function(){
+  $("nav ul li a,.back_to_top a,.footer_top a,a.button").click(function(e){
     // console.log($(this.hash).offset().top)
-    thisElem = $(this.hash);
-    //body영역과 스크롤 영역의 조절;
+    var thisElem = $(this.hash);
     $("html,body").stop();
-    $("html,body").animate({scrollTop : thisElem.offset().top},1500);//속성 초단위 esing값
+    $("html,body").animate({scrollTop : thisElem.offset().top},1500);
+    //e.preventDefault();
+    return false;
   });
-  return false;
 
+  /*scroll 상단 이동버튼 노출*/
+  $(window).scroll(function(){
+    // console.log($(this).scrollTop());
+    if($(this).scrollTop() == 0){
+      $(".back_to_top").removeClass("on");
+    }else{
+      $(".back_to_top").addClass("on");
+    }
+  });
 });
