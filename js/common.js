61eaa43d0ba7b0ae16e369f23138ffa29edd50fa
xihyeon <xihyeon.lee@gmail.com>
2020-11-18 16:36:12 +0900

    스크립트

--- js/common.js
+++ js/common.js
@@ -1,3 +1,13 @@
 $(function(){
+  //script 입력영역
+    var thisElem
+  $("nav ul li a,.footer_top a,a.button").click(function(){
+    // console.log($(this.hash).offset().top)
+    thisElem = $(this.hash);
+    //body영역과 스크롤 영역의 조절;
+    $("html,body").stop();
+    $("html,body").animate({scrollTop : thisElem.offset().top},1500);//속성 초단위 esing값
+  });
+  return false;
 
 });
